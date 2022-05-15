ef08c27c3362ad2ceed982339a08ad02e4dbbb45
msla1219 <76463854+msla1219@users.noreply.github.com>
2022-05-15 22:14:43 +0900

    Add files via upload

--- caesar.py
+++ caesar.py
@@ -1,31 +1,24 @@
 def encrypt(key, plaintext):
     ciphertext = ""
 
-    for c in plaintext:
+    # loop to encrypt one character by character
+    for each_char in plaintext:
 
-        # check if character is an uppercase letter
-        if c.isupper():
+        # only in case of uppercase
+        if each_char.isupper():
 
-            # find the position in 0-25
-            c_unicode = ord(c)
+            # store the offset (relative position) from "A"
+            offset = ord(each_char) - ord("A")
 
-            c_index = ord(c) - ord("A")
+            # move the position by the key
+            new_offset = (offset + key) % 26
 
-            # perform the key
-            new_index = (c_index + key) % 26
+            # append to encrypted character to the string under the encryption
+            ciphertext = ciphertext + chr(ord("A") + new_offset)
 
-            # convert to new character
-            new_unicode = new_index + ord("A")
-
-            new_character = chr(new_unicode)
-
-            # append to encrypted string
-            ciphertext = ciphertext + new_character
-
+        # just do nothing in case of not uppercase
         else:
-
-            # since character is not uppercase, leave it as it is
-            ciphertext += c
+            ciphertext = ciphertext + each_char
 
     return ciphertext
 
@@ -33,30 +26,23 @@ def encrypt(key, plaintext):
 def decrypt(key, ciphertext):
     plaintext = ""
 
-    for c in ciphertext:
+    # loop to decrypt one character by character
+    for each_char in ciphertext:
 
-        # check if character is an uppercase letter
-        if c.isupper():
+        # only in case of uppercase
+        if each_char.isupper():
 
-            # find the position in 0-25
-            c_unicode = ord(c)
+            # store the offset (relative position) from "A"
+            offset = ord(each_char) - ord("A")
 
-            c_index = ord(c) - ord("A")
+            # move the position reversely by the key
+            new_offset = (offset - key) % 26
 
-            # perform the key
-            new_index = (c_index - key) % 26
+            # append to decrypted character to the string under the decryption
+            plaintext = plaintext + chr(ord("A") + new_offset)
 
-            # convert to new character
-            new_unicode = new_index + ord("A")
-
-            new_character = chr(new_unicode)
-
-            # append to encrypted string
-            plaintext = plaintext + new_character
-
+        # just do nothing in case of not uppercase
         else:
-
-            # since character is not uppercase, leave it as it is
-            plaintext = plaintext + c
+            plaintext = plaintext + each_char
 
     return plaintext
