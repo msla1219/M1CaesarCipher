4d013f23f405f6669e47320c0cc6025cc7642219
msla1219 <76463854+msla1219@users.noreply.github.com>
2022-05-15 21:26:48 +0900

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,62 @@
+def encrypt(key,plaintext):
+    ciphertext=""
+    #YOUR CODE HERE
+
+    for c in plaintext:
+
+        # check if character is an uppercase letter
+        if c.isupper():
+
+            # find the position in 0-25
+            c_unicode = ord(c)
+
+            c_index = ord(c) - ord("A")
+
+            # perform the key
+            new_index = (c_index + key) % 26
+
+            # convert to new character
+            new_unicode = new_index + ord("A")
+
+            new_character = chr(new_unicode)
+
+            # append to encrypted string
+            encryption = encryption + new_character
+
+        else:
+
+            # since character is not uppercase, leave it as it is
+            encryption += c
+
+    return ciphertext
+
+def decrypt(key,ciphertext):
+    plaintext=""
+    
+    for c in ciphertext:
+
+        # check if character is an uppercase letter
+        if c.isupper():
+
+            # find the position in 0-25
+            c_unicode = ord(c)
+
+            c_index = ord(c) - ord("A")
+
+            # perform the key
+            new_index = (c_index - key) % 26
+
+            # convert to new character
+            new_unicode = new_index + ord("A")
+
+            new_character = chr(new_unicode)
+
+            # append to encrypted string
+            plaintext = plaintext + new_character
+
+        else:
+
+            # since character is not uppercase, leave it as it is
+            plaintext = plaintext + c
+    #YOUR CODE HERE
+    return plaintext
